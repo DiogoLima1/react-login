a315e370a57516b787ace5631127d5f11391d8c4
DiogoLima1 <62252469+DiogoLima1@users.noreply.github.com>
2020-04-02 20:28:22 -0300

    Login
    tentativa 1

---
+++ src/components/login.component.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Login</h3>
+                
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Digite seu email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Senha</label>
+                    <input type="password" className="form-control" placeholder="Digite sua senha" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Entrar</button>
+                <br></br>
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Lembrar de mim</label>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
